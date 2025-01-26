fbc4ea46c28ad7981dd35a69a3e6fd55b59feb12
taurasbear <youngicorn@gmail.com>
2025-01-26 17:53:39 +0200

    initialised winsock on server. Created socket and binded on server.

--- server.c
+++ server.c
@@ -4,15 +4,17 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
+#define DEFAULT_PORT 27015
+
 int main()
 {
     WSADATA wsadata;
-    int result;
-    result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+    int iResult;
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsadata);
 
-    if (result != 0)
+    if (iResult != 0)
     {
-        printf("WSAStartup failed: %d", result);
+        printf("WSAStartup failed: %d", iResult);
         return 1;
     }
 
@@ -21,9 +23,43 @@ int main()
                     hints;
 
     ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_protocol = AI_PASSIVE;
+
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0)
+    {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET)
+    {
+        printf("error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    iResult = bind(ListenSocket, result->ai_addr, result->ai_addrlen);
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("bind failed: %d", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(result); // addr info is no longer necessary because bind was successful
+
+    
 
     return 0;
 }
