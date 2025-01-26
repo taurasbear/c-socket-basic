f1aaa7fd1cf45e276428d197d477c99991af4d37
taurasbear <youngicorn@gmail.com>
2025-01-26 16:58:06 +0200

    created a socket. Connected to a socket

--- client.c
+++ client.c
@@ -4,7 +4,9 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-int main()
+#define DEFAULT_PORT 27015
+
+int main(int argc, char *argv[])
 {
     WSADATA wsadata;
     int iResult;
@@ -13,6 +15,51 @@ int main()
     if (iResult != 0)
     {
         printf("WSAStartup failed: %d", iResult);
+        return 1;
+    }
+
+    struct addrinfo *result = NULL, *ptr = NULL, hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+
+    iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
+    if (iResult != 0)
+    {
+        printf("getaddrinfo failed: %d", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    ptr = result; // attempting to connect to first address returned by getaddrinfo
+    ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+    if (ConnectSocket == INVALID_SOCKET)
+    {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    iResult = connect(ConnectSocket, ptr->ai_addr, ptr->ai_addrlen);
+
+    if (iResult == SOCKET_ERROR)
+    {
+        closesocket(ConnectSocket);
+        ConnectSocket = INVALID_SOCKET;
+    }
+
+    // should try next socket in linked list
+
+    freeaddrinfo(result);
+
+    if (ConnectSocket == INVALID_SOCKET)
+    {
+        printf("Unable to connect to server.\n");
+        WSACleanup();
         return 1;
     }
 
